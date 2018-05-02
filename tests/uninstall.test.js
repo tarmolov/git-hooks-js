e83af69d6c80979e8ca603417836df5a90fa16ce
Robert (Jamie) Munro <rjmunro@arjam.net>
2018-05-02 19:51:53 +0100

    Use existence of backup as marker for installation (#54)
    * Use existence of backup as marker for installation
    
    * Always make a backup when installing
    * Always check for backup when uninstalling
    
    * Fix tests for node 0.10 which has no execSync

--- tests/uninstall.test.js
+++ tests/uninstall.test.js
@@ -1,15 +1,22 @@
 require('chai').should();
+var exec = require('child_process').exec;
 var gitHooks = require('../lib/git-hooks');
 var fsHelpers = require('../lib/fs-helpers');
 
-var SANDBOX_PATH = __dirname + '/tmp-sandbox/';
+var SANDBOX_PATH = '/tmp/tmp-sandbox/';
 var GIT_ROOT = SANDBOX_PATH + '.git/';
 var GIT_HOOKS = GIT_ROOT + 'hooks';
 var GIT_HOOKS_OLD = GIT_ROOT + 'hooks.old';
 
 describe('--uninstall', function () {
-    beforeEach(function () {
-        fsHelpers.makeDir(GIT_ROOT);
+    beforeEach(function (done) {
+        fsHelpers.makeDir(SANDBOX_PATH);
+        exec('git init', {cwd: SANDBOX_PATH}, function (err) {
+            if (err) {
+                throw err;
+            }
+            done();
+        });
     });
 
     afterEach(function () {
@@ -39,19 +46,18 @@ describe('--uninstall', function () {
     });
 
     describe('when backup is absent', function () {
-        beforeEach(function () {
-            fsHelpers.makeDir(GIT_HOOKS);
-        });
 
         it('should not remove hooks directory', function () {
-            gitHooks.uninstall(SANDBOX_PATH);
+            var fn = function () {
+                gitHooks.uninstall(SANDBOX_PATH);
+            };
+            fn.should.throw(Error);
             fsHelpers.exists(GIT_HOOKS).should.be.true;
         });
     });
 
     describe('when backup exists', function () {
         beforeEach(function () {
-            fsHelpers.makeDir(GIT_HOOKS);
             fsHelpers.makeDir(GIT_HOOKS_OLD);
         });
 
